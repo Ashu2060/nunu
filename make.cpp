a699079493c93e8b7e9165fb1684ad311330f397
name <ajha49254@gmail.com>
2026-03-05 21:22:44 +0530

    new

---
+++ make.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int  num;
+  cout<<"Enter a number: ";
+  cin>>num;
+  int rem,ans=0,mul=1;
+  while(num>0){
+    //reminder
+    rem%=2;
+    //quotient
+    num/=2;
+    //answer
+    ans=rem*mul+ans;
+    //multily
+    mul*=10;
+
+  }
+  cout<<"Binary number is: "<<ans;
+}
